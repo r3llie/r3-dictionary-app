ba11418c1c3cbf9b2ae3d5f40e544f46be4cd517
r3llie <ghailstone503@gmail.com>
2022-01-29 12:58:16 -0800

    Started to display definition but got blocked by api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import Results from "./Results";
 import axios from "axios";
 
 export default function Dictionary() {
 let [keyword, setKeyword] = useState("");
+let [results, setResults] = useState(null);
 
 function handleResponse(response) {
-    console.log(response.data[0]);
+    console.log(response.data[0].meanings[0].definitions[0].definition);
+    setResults(response.data[0]);
 }
 
 function search(event) {
@@ -23,5 +26,6 @@ setKeyword(event.target.value);
       <form onSubmit={search}>
           <input type="search"  onChange={handleKeywordChange}/>
       </form>
+      <Results results={results}/>
       </div>;
 }
