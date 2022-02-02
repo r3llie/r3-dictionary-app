0aaf85285603ba700e478a616c9ed0491bc46b5b
r3llie <ghailstone503@gmail.com>
2022-02-02 14:25:19 -0800

    Fixed some warnings

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,10 +1,9 @@
-import react from "react";
 
 export default function Phonetic(props) {
   console.log(props.phonetic);
   return (
     <div className="Phonetic">
-      <a href={props.phonetic.audio} target="_blank">
+      <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
         Listen
       </a>
       <br />
