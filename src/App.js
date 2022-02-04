8bc1590256e6e5743932640b07c5176b47b07acd
r3llie <ghailstone503@gmail.com>
2022-02-04 11:40:03 -0800

    Set default keyword

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ export default function App() {
           <img src={logo} className="App-logo img-fluid" alt="logo" />
         </header>
         <main>
-          <Dictionary />
+          <Dictionary defaultKeyword="autism"/>
         </main>
         <footer className="App-footer">
           <small>Coded by @r3llie</small>
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -3,34 +3,51 @@ import "./Dictionary.css";
 import Results from "./Results";
 import axios from "axios";
 
-export default function Dictionary() {
-  let [keyword, setKeyword] = useState("");
+export default function Dictionary(props) {
+  let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
+  let [loaded, setLoaded] = useState(false);
 
   function handleResponse(response) {
     setResults(response.data[0]);
   }
 
-  function search(event) {
-    event.preventDefault();
+  function Search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    Search();
   }
 
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
 
-  return (
-    <div className="Dictionary">
-      <section>
-        <form onSubmit={search} className="form">
-          <input type="search" onChange={handleKeywordChange} />
-        </form>
-        <div className="hint">Search for words like dance, music, cup, summer, joke...</div>
-      </section>
+ function load() {
+     setLoaded(true);
+     Search();
+ }
 
-      <Results results={results} />
-    </div>
-  );
+  if (loaded) {
+    return (
+      <div className="Dictionary">
+        <section>
+          <form onSubmit={handleSubmit} className="form">
+            <input type="search" onChange={handleKeywordChange} />
+          </form>
+          <div className="hint">
+            Search for words like dance, music, cup, summer, joke...
+          </div>
+        </section>
+
+        <Results results={results} />
+      </div>
+    );
+  } else {
+      load()
+      return "Loading";
+  }
 }
