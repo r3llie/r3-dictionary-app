e9c87c739b5c8007c270d7fe1e5a1cccc5ae83ca
r3llie <ghailstone503@gmail.com>
2022-02-03 16:43:16 -0800

    Started working on the css

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -4,28 +4,33 @@ import Results from "./Results";
 import axios from "axios";
 
 export default function Dictionary() {
-let [keyword, setKeyword] = useState("");
-let [results, setResults] = useState(null);
+  let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
-function handleResponse(response) {
-    console.log(response.data[0].meanings[0].definitions[0].definition);
+  function handleResponse(response) {
     setResults(response.data[0]);
-}
+  }
 
-function search(event) {
-event.preventDefault();    
-let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-axios.get(apiUrl).then(handleResponse);
-}
+  function search(event) {
+    event.preventDefault();
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
 
-function handleKeywordChange(event) {
-setKeyword(event.target.value);
-}
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
 
-  return <div className="Dictionary">
-      <form onSubmit={search} className="form">
-          <input type="search"  onChange={handleKeywordChange}/>
-      </form>
-      <Results results={results}/>
-      </div>;
+  return (
+    <div className="Dictionary">
+      <section>
+        <form onSubmit={search} className="form">
+          <input type="search" onChange={handleKeywordChange} />
+        </form>
+        <div className="hint">Search for words like dance, music, cup, summer, joke...</div>
+      </section>
+
+      <Results results={results} />
+    </div>
+  );
 }
--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,6 +1,5 @@
 
 export default function Phonetic(props) {
-  console.log(props.phonetic);
   return (
     <div className="Phonetic">
       <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
